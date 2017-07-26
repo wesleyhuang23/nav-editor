e6d49e497d2c8297235445337b55bdb27414b4ac
Wesley <wesley@gooddogdesign.com>
2017-07-26 10:42:11 -0700

    added home header

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <h1>Navigation</h1>
+    <a class="btn-success">Add Nav</a>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .btn-success{
+    padding:5px;
+    border-radius: 5px;
+  }
+
+</style>
