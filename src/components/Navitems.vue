6780206c635322f4996b616751bdd506c61d8fd7
Wesley <wesley@gooddogdesign.com>
2017-07-27 16:38:49 -0700

    able to add new empty navitem

--- src/components/Navitems.vue
+++ src/components/Navitems.vue
@@ -14,7 +14,7 @@
             <div class="row">
                 <div class="col-lg-6">
                     <h1>Nav Items</h1>
-                    <button class="btn btn-default">Add Nav Item</button>
+                    <button class="btn btn-default" v-on:click="addItem()">Add Nav Item</button>
                 </div>
                 <div class="col-lg-6 navitems">
                     <table>
@@ -23,7 +23,7 @@
                             <th>Link</th>
                         </tr>
 
-                        <tr v-for="items in navitems">
+                        <tr v-for="items in navitems" class="navData">
                             <td><input type="text" v-bind:value="items.name"></td>
                             <td><input type="text" v-bind:value="items.link"></td>
                             <td><a class="btn btn-default btn-sm" v-on:click="removeItem(items.name)">
@@ -36,8 +36,8 @@
             </div>
             <hr>
             <div class="row actions">
-                <button class="btn btn-danger"> Delete</button>
-                <button class="btn btn-success"> Save</button>
+                <button class="btn btn-danger">Delete</button>
+                <button class="btn btn-success" v-on:click="update()">Save</button>
             </div>
         </div>
     </div>
@@ -51,7 +51,7 @@ export default {
                 { name: 'outerwear', link: 'www.guideboat.co/womens/outerwear'},
                 { name: 'shoes', link: 'www.guideboat.co/womens/shoes'},
                 { name: 'coats', link: 'www.guideboat.co/womens/coats'}
-            ]
+            ],
         }
     },
     methods: {
@@ -61,6 +61,27 @@ export default {
                     this.navitems.splice(i, 1);
                 }
             }
+        },
+        update: function(){
+            var data = document.getElementsByClassName('navData');
+            
+            for(let i = 0; i < data.length; i++){
+                this.navitems.push({ name: data[i].cells[0].children[0]._value, link: this.navitems[i].link = data[i].cells[1].children[0]._value })
+            }
+            console.log(this.navitems.length);
+            for(var j = 0; j < (this.navitems.length / 2) + 1; j++){
+                this.navitems.splice(j, 1);
+            }
+            // this.update_local_storage();
+            // console.log(data);
+            // console.log(data[0].cells[0].children[0]._value);
+            // console.log(data[0].cells[1].children[0]._value);
+        },
+        addItem: function(){
+            this.navitems.push({name: '', link: ''});
+        },
+        update_local_storage: function(){
+            console.log(this.navitems);
         }
     }
 }
