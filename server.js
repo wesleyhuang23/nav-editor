038f163af88174b26e5a372ae56b66b8ee5093e0
Wesley <wesleyhuang23@gmail.com>
2017-08-07 13:07:54 -0700

    mongodb setup

--- server.js
+++ server.js
@@ -1,17 +1,43 @@
 var express = require('express');
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-
 var app = express();
 
-app.use(express.static('dist'));
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var url = 'mongodb://localhost:4000';
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server.");
-  db.close();
+// var configDB = require('./config/database.js');
+mongoose.connect('mongodb://localhost:27017');
+mongoose.connection.once('open',() => console.log('Connected to Mongo'));
+var db = mongoose.connection;
+
+
+var usersSchema = mongoose.Schema({
+  name: String
 });
+
+var Users = mongoose.model('Users', usersSchema);
+
+var john = new Users({name: 'John'});
+console.log(john);
+Users.find(function (err, Users) {
+  if (err) return console.error(err);
+  console.log(Users);
+});
+
+// app.use(morgan('dev'));
+// app.use(cookieParser());
+app.use(bodyParser());
+
+// app.use(session({ secret: 'ilsdkjfhgasdkg823tr7' }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(flash());
+
+app.use(express.static('dist'));
 
 app.listen(4000, function(){
   console.log('listening on port 4000...')
